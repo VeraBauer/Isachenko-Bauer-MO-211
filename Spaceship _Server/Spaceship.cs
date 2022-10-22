656727792a9d2a093af1704a13d02a124941157e
VaiceIVE <vanisachenko@gmail.com>
2022-10-22 16:08:08 +0600

    Completed Lab 1 fitst part

--- Spaceship _Server/Spaceship.cs
+++ Spaceship _Server/Spaceship.cs
@@ -3,10 +3,265 @@ using System.Collections.Generic;
 
 namespace Spaceship__Server
 {
+    using System;
+
+public class Vector
+
+    {
+        public List<int> coords { get; set; } = new List<int>();
+
+        public Vector(List<int> lis)
+
+        {
+
+            for (int i = 0; i < lis.Count; i++)
+
+            {
+
+                coords.Add(lis[i]);
+
+            }
+
+        }
+
+        public Vector(params int[] args)
+
+        {
+
+            for (int i = 0; i < args.Length; i++)
+
+            {
+
+                coords.Add(args[i]);
+
+            }
+
+        }
+
+        public override string ToString()
+
+        {
+
+            string str = "";
+
+            str += "Vector(";
+
+            for (int i = 0; i < coords.Count; i++)
+
+            {
+
+                if (i != coords.Count - 1)
+
+                {
+
+                    str += coords[i] + ", ";
+
+                }
+
+                else
+
+                {
+
+                    str += coords[i];
+
+                }
+
+            }
+
+            str += ")";
+
+            return str;
+
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Vector vector))
+            {
+                return false;
+            }
+
+            if (vector.coords.Count != this.coords.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < this.coords.Count; i++)
+            {
+                if (this.coords[i] != vector.coords[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(coords, this.coords.Count);
+        }
+
+        public int this[int index]
+
+        {
+
+            get
+
+            {
+                if (index <= this.coords.Count)
+                {
+                    return coords[index];
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+
+            }
+            set
+
+            {
+                if (index <= this.coords.Count)
+                {
+                    coords[index] = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+
+            }
+
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+
+        {
+
+            if (a.coords.Count != b.coords.Count)
+
+            {
+
+                throw new ArgumentException();
+
+            }
+
+            else
+
+            {
+
+                List<int> ansvec = new List<int>();
+
+                for (int i = 0; i < a.coords.Count; i++)
+
+                {
+
+                    ansvec.Add(a[i] + b[i]);
+
+                }
+
+                Vector ans = new Vector(ansvec);
+
+                return ans;
+
+            }
+
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+
+        {
+
+            if (a.coords.Count != b.coords.Count)
+
+            {
+
+                throw new ArgumentException();
+
+            }
+
+            else
+
+            {
+
+                List<int> ansvec = new List<int>();
+
+                for (int i = 0; i < a.coords.Count; i++)
+
+                {
+
+                    ansvec.Add(a[i] - b[i]);
+
+                }
+
+                Vector ans = new Vector(ansvec);
+
+                return ans;
+
+            }
+
+        }
+
+        public static bool operator ==(Vector a, Vector b)
+
+        {
+
+            int size = Math.Max(a.coords.Count, b.coords.Count);
+
+            if (a.coords.Count != b.coords.Count)
+
+            {
+
+                return false;
+
+            }
+
+            for (int i = 0; i < size; i++)
+
+            {
+
+                if (a[i] != b[i])
+
+                {
+
+                    return false;
+
+                }
+
+            }
+
+            return true;
+
+        }
+
+        public static bool operator !=(Vector a, Vector b)
+
+        {
+
+            if (a == b)
+
+            {
+
+                return false;
+
+            }
+
+            else
+
+            {
+
+                return true;
+
+            }
+
+        }
+    }
+
     public interface IMovable
     {
-        List<int> Speed { get;}
-        List<int> Position { get; set; }
+        Vector Speed { get;}
+        Vector Position { get; set; }
     }
 
     public interface ICommand
@@ -24,17 +279,16 @@ namespace Spaceship__Server
 
         public void Execute()
         {
-            if (_obj.Speed.Count != _obj.Position.Count)
+            if (_obj.Speed.coords.Count != _obj.Position.coords.Count)
             {
                 throw new Exception();
             }
-            List<int> newpos = _obj.Position;
-            for (int i = 0; i < _obj.Position.Count; i++)
+            Vector newpos = _obj.Position;
+            for (int i = 0; i < _obj.Position.coords.Count; i++)
             {
                 newpos[i] += _obj.Speed[i];
             }
             _obj.Position = newpos;
         }
-
     }
 }
