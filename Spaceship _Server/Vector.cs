756c6c36243bb7e375946d7ba78cabf6c15acf14
Vera <vera.bauer@bk.ru>
2022-12-15 13:34:09 +0600

    Splited functionality

--- Spaceship _Server/Vector.cs
+++ Spaceship _Server/Vector.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Spaceship__Server
 {
-    using System;
-
-public class Vector
+    public class Vector
 
     {
         public List<int> coords { get; set; } = new List<int>();
@@ -256,40 +257,6 @@ public class Vector
 
             }
 
-        }
-    }
-
-    public interface IMovable
-    {
-        Vector Speed { get;}
-        Vector Position { get; set; }
-    }
-
-    public interface ICommand
-    {
-        public void Execute();
-    }
-
-    public class MoveCommand : ICommand
-    {
-        IMovable _obj;
-        public MoveCommand(IMovable obj)
-        {
-            _obj = obj;
-        }
-
-        public void Execute()
-        {
-            if (_obj.Speed.coords.Count != _obj.Position.coords.Count)
-            {
-                throw new Exception();
-            }
-            Vector newpos = _obj.Position;
-            for (int i = 0; i < _obj.Position.coords.Count; i++)
-            {
-                newpos[i] += _obj.Speed[i];
-            }
-            _obj.Position = newpos;
         }
     }
 }
