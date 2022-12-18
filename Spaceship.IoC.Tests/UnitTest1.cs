afbf87967b1fbade1391ae23dba05777800ce9ca
Self <self@Vivo.ru>
2022-12-18 20:51:07 +0600

    First commits to continious movement

---
+++ Spaceship.IoC.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Hwdtech;
+
+namespace Spaceship.IoC.Tests;
+
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        InitScopeBasedIoCImplimentationCommand().Execute();
+    }
+}
