47ae68e51b06b9d5cd28ed91f9efedea2a118cc3
Self <self@Vivo.ru>
2022-12-28 09:22:36 +0600

    100% code coverage

--- Spaceship.Macro.Test/MacroCreatorTests.cs
+++ Spaceship.Macro.Test/MacroCreatorTests.cs
@@ -56,4 +56,33 @@ public class MacroBuilderTest
 
         Assert.Equal(2, _queue.Count);
     }
+
+    [Fact]
+    public void PushCommandTest()
+    {
+        Queue<Spaceship__Server.ICommand> queue = new();
+
+        Mock<Spaceship__Server.ICommand> cmd = new();
+
+        PushCommand pc = new(cmd.Object, queue);
+
+        pc.Execute();
+
+        Assert.Single(queue);
+    }
+    [Fact]
+    public void GetSpeedTest()
+    {
+        Mock<IUObject> obj = new();
+
+        obj.Setup(o => o.get_property("Position")).Returns(new Vector(2));
+
+        obj.Setup(o => o.get_property("Velocity")).Returns(new Vector(1));
+
+        IMovable movable = IoC.Resolve<IMovable>("Adapters.IUObject.MoveCommand", obj.Object);
+
+        Assert.Equal(movable.Speed, new Vector(1));
+
+
+    }
 }
