043af49a097833d60468c9fe9eb3a017ceb820ed
Vera <vera.bauer@bk.ru>
2023-01-12 16:24:56 +0600

    lab4 first try

---
+++ Spaceship _Server/PushCommand.cs
@@ -0,0 +1,19 @@
+﻿using Hwdtech;
+using System.Collections.Generic;
+
+namespace Spaceship__Server
+{
+    public class PushCommand : ICommand
+    {
+        Spaceship__Server.ICommand comd = null;
+        public PushCommand (Spaceship__Server.ICommand cmd)
+        {
+            comd = cmd;
+        }
+        public void Execute()
+        {
+            Hwdtech.IoC.Resolve<Queue<Spaceship__Server.ICommand>>("IoC.GetQueue").Enqueue(comd);
+            Hwdtech.IoC.Resolve<Queue<Spaceship__Server.ICommand>>("IoC.GetQueue").Enqueue(this);
+        }
+    }
+}
