edbd51089e383e93005fe33dc39cd1d0fbdd53a1
Isachenko Ivan <vanisachenko@gmail.com>
2023-04-29 18:34:06 +0600

    Serialization

--- Spaceship.IoC.Test.No.Strategies/EndpointTests.cs
+++ Spaceship.IoC.Test.No.Strategies/EndpointTests.cs
@@ -474,7 +474,14 @@ public class EndTests
     [Fact]
     public void ContractTest()
     {
+        Dictionary<string, object> ValueDictionary = new(){{"type", "Shoot"}, {"gameid", "1"}, {"objid", "obj123"}, {"thread", "2"}};
+
         JSONContract dto =  new();
+
+        JsonDictionary jd = new(ValueDictionary);
+
+        dto.Value = jd;
+
         MemoryStream mem = new MemoryStream();
         try
         {
