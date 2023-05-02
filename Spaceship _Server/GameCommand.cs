aa82c7aff38ed5430aaaff3412330c35977d20a2
Ivan Isachenko <109348025+VaiceIVE@users.noreply.github.com>
2023-05-02 09:17:42 +0600

    Update GameCommand.cs

--- Spaceship _Server/GameCommand.cs
+++ Spaceship _Server/GameCommand.cs
@@ -20,7 +20,9 @@ public class GameCommand : Spaceship__Server.ICommand
         {
             try
             {
+            
                 Spaceship__Server.ICommand cmd = IoC.Resolve<Spaceship__Server.ICommand>("Game.Current.HandleCommand");
+                
                 if (cmd != null)
                 {
                     cmd.Execute();
