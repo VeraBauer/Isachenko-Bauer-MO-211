b2f71a739c9901d8b429876e56d838e81ba3cc52
Isachenko Ivan <vanisachenko@gmail.com>
2023-05-29 20:37:30 +0600

    fixed \t

--- Spaceship.IoC.Test.No.Strategies/AdapterGeneratorTests.cs
+++ Spaceship.IoC.Test.No.Strategies/AdapterGeneratorTests.cs
@@ -9,6 +9,6 @@ public class AdapterGeneratorTests
     [Fact]
     public void MainTestMovable()
     {
-        Assert.Equal( "public class IMovableAdapter : IMovable\n{\n         public Spaceship__Server.Vector Speed { get; }\n         public Spaceship__Server.Vector Position { get;  set; }\n         public IMovableAdapter (object obj)\n         {\n                 this.Speed = Hwdtech.IoC.Resolve<Spaceship__Server.Vector>(\"IUObject.Property.Get\", obj, \"Speed\");\n                 this.Position = Hwdtech.IoC.Resolve<Spaceship__Server.Vector>(\"IUObject.Property.Get\", obj, \"Position\");\n         }\n}", AdapterGenerator.Generate(typeof(IMovable))) ;
+        Assert.Equal( "public class IMovableAdapter : IMovable\n{\n\t public Spaceship__Server.Vector Speed { get; }\n\t public Spaceship__Server.Vector Position { get;  set; }\n\t public IMovableAdapter (object obj)\n\t {\n\t\t this.Speed = Hwdtech.IoC.Resolve<Spaceship__Server.Vector>(\"IUObject.Property.Get\", obj, \"Speed\");\n\t\t this.Position = Hwdtech.IoC.Resolve<Spaceship__Server.Vector>(\"IUObject.Property.Get\", obj, \"Position\");\n\t }\n}", AdapterGenerator.Generate(typeof(IMovable))) ;
     }
 }
